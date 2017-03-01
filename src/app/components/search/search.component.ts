5c5469d500a06916dac761ad819b02cf5080cdec
oswaldosalazar <oswaldo.r.salazar@gmail.com>
2017-03-01 15:09:33 -0700

    Added getDate function for API query

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -13,12 +13,27 @@ export class SearchComponent implements OnInit {
   searchUrl : string;
   resultsJson : any;
   resultsArray : any;
+  today: string;
+  year: Date;
+  month: string;
+  day: string;
+  currentDate: Date;
+  formattedDate: string;
 
 
   constructor( 
     private geolocation: GeolocationService,
     private http: Http
                ) { }
+
+  getDate() {
+    this.currentDate = new Date;
+    let year = this.currentDate.getFullYear();
+    let mm = (this.currentDate.getMonth() + 1);
+    let dd = (this.currentDate.getDate());
+    console.log([year,(mm>9?'':'0')+mm,(dd>9?'':'0')+dd].join(''));
+    return [year,(mm>9?'':'0')+mm,(dd>9?'':'0')+dd].join('');
+  }
 
   getCurrentPosition() {
     if(navigator.geolocation) {
@@ -28,7 +43,7 @@ export class SearchComponent implements OnInit {
           this.latitude = position.coords.latitude.toString();
           this.longitude = position.coords.longitude.toString();
           this.locationString =  this.latitude + "," + this.longitude;
-          this.searchUrl = "https://api.foursquare.com/v2/venues/explore?ll=" + this.locationString + "&client_id=NHF0X5EXQLHYJ3IG5FIYSJYD2R33BLQSKGGQUBSIYMXWFYA4&client_secret=5TRQLKFODOFFJW55T0FHBH3BWNW3RFAOBK24BK2BSPB2QD3C&v=20170227&section=food&openNow=1";
+          this.searchUrl = "https://api.foursquare.com/v2/venues/explore?ll=" + this.locationString + "&client_id=NHF0X5EXQLHYJ3IG5FIYSJYD2R33BLQSKGGQUBSIYMXWFYA4&client_secret=5TRQLKFODOFFJW55T0FHBH3BWNW3RFAOBK24BK2BSPB2QD3C&v="+this.getDate()+"&section=food&openNow=1";
           this.http.get(this.searchUrl)
           .subscribe(
             (data: any) => {
@@ -43,6 +58,7 @@ export class SearchComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.getDate();
     this.getCurrentPosition();
   }
 
