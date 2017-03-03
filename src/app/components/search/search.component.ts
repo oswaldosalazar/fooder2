a9b29b5f9ac6bc1f1802298ee2009dc81894c2c3
oswaldosalazar <oswaldo.r.salazar@gmail.com>
2017-03-02 17:55:15 -0700

    Working on pix urls

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -14,6 +14,8 @@ export class SearchComponent implements OnInit {
   resultsJson : any;
   resultsArray : any;
   currentDate: Date;
+  // final: any;
+  cardTypes: any;
 
   constructor( 
     private geolocation: GeolocationService,
@@ -28,7 +30,7 @@ export class SearchComponent implements OnInit {
     return [year,(mm>9?'':'0')+mm,(dd>9?'':'0')+dd].join('');
   }
 
-  getCurrentPosition() {
+  getNearRestaurants() {
     if(navigator.geolocation) {
       this.geolocation.getLocation()
       .subscribe(
@@ -42,6 +44,20 @@ export class SearchComponent implements OnInit {
             (data: any) => {
               this.resultsJson = data.json();
               this.resultsArray = this.resultsJson.response.groups[0].items;
+              this.resultsArray.map( (elem) => {
+                var venueSearchUrl = "https://api.foursquare.com/v2/venues/"+elem.venue.id+"/photos?client_id=NHF0X5EXQLHYJ3IG5FIYSJYD2R33BLQSKGGQUBSIYMXWFYA4&client_secret=5TRQLKFODOFFJW55T0FHBH3BWNW3RFAOBK24BK2BSPB2QD3C&v=2"+this.getDate();
+                this.http.get(venueSearchUrl)
+                .subscribe((venuePicsUrl) => {
+                  let final: any;
+                  console.log(venuePicsUrl.json());
+                  // final.venueId = elem.venue.id;
+                  // final.name = elem.venue.name;
+                  // final.hours = elem.venue.hours;
+                  // final.address = elem.venue.location.address;
+                  // final.image = venuePicsUrl.json().data.response.photos.items[2].prefix+'300x400'+venuePicsUrl.json().data.response.photos.items[2].suffix;
+                  // this.cardTypes.push(final);
+                })
+              })
             }
           )
         }
@@ -50,7 +66,7 @@ export class SearchComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.getCurrentPosition();
+    this.getNearRestaurants();
   }
 
 }
