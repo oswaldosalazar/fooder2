ce607b2c3fbf5428b219fce9e00cb49612e6df30
oswaldosalazar <oswaldo.r.salazar@gmail.com>
2017-03-03 12:18:57 -0700

    Cards listed on repeat

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -14,8 +14,7 @@ export class SearchComponent implements OnInit {
   resultsJson : any;
   resultsArray : any;
   currentDate: Date;
-  // final: any;
-  cardTypes: any;
+  cardTypes: any = [];
 
   constructor( 
     private geolocation: GeolocationService,
@@ -38,6 +37,7 @@ export class SearchComponent implements OnInit {
           this.latitude = position.coords.latitude.toString();
           this.longitude = position.coords.longitude.toString();
           this.locationString =  this.latitude + "," + this.longitude;
+          
           this.searchUrl = "https://api.foursquare.com/v2/venues/explore?ll=" + this.locationString + "&client_id=NHF0X5EXQLHYJ3IG5FIYSJYD2R33BLQSKGGQUBSIYMXWFYA4&client_secret=5TRQLKFODOFFJW55T0FHBH3BWNW3RFAOBK24BK2BSPB2QD3C&v="+this.getDate()+"&section=food&openNow=1";
           this.http.get(this.searchUrl)
           .subscribe(
@@ -45,17 +45,17 @@ export class SearchComponent implements OnInit {
               this.resultsJson = data.json();
               this.resultsArray = this.resultsJson.response.groups[0].items;
               this.resultsArray.map( (elem) => {
-                var venueSearchUrl = "https://api.foursquare.com/v2/venues/"+elem.venue.id+"/photos?client_id=NHF0X5EXQLHYJ3IG5FIYSJYD2R33BLQSKGGQUBSIYMXWFYA4&client_secret=5TRQLKFODOFFJW55T0FHBH3BWNW3RFAOBK24BK2BSPB2QD3C&v=2"+this.getDate();
+                let venueSearchUrl = "https://api.foursquare.com/v2/venues/"+elem.venue.id+"/photos?client_id=NHF0X5EXQLHYJ3IG5FIYSJYD2R33BLQSKGGQUBSIYMXWFYA4&client_secret=5TRQLKFODOFFJW55T0FHBH3BWNW3RFAOBK24BK2BSPB2QD3C&v="+this.getDate();
                 this.http.get(venueSearchUrl)
                 .subscribe((venuePicsUrl) => {
-                  let final: any;
-                  console.log(venuePicsUrl.json());
-                  // final.venueId = elem.venue.id;
-                  // final.name = elem.venue.name;
-                  // final.hours = elem.venue.hours;
-                  // final.address = elem.venue.location.address;
-                  // final.image = venuePicsUrl.json().data.response.photos.items[2].prefix+'300x400'+venuePicsUrl.json().data.response.photos.items[2].suffix;
-                  // this.cardTypes.push(final);
+                  let final:any = {};
+                  final.venueId = elem.venue.id;
+                  final.name = elem.venue.name;
+                  final.rating = elem.venue.rating;
+                  final.hours = elem.venue.hours;
+                  final.address = elem.venue.location.address;
+                  final.image = venuePicsUrl.json().response.photos.items[0].prefix+'300x200'+venuePicsUrl.json().response.photos.items[0].suffix;
+                  this.cardTypes.push(final);
                 })
               })
             }
